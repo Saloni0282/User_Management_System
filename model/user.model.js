edb3048b238b6b09ee091d543bc6adc41421628f
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-11-01 22:01:40 +0530

    Completed UserManagementSystem

--- model/user.model.js
+++ model/user.model.js
@@ -1,10 +1,10 @@
-const mongoose  = require('mongoose') ;
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    first_name:{type: String, required: true},
+    first_name: { type: String, required: true },
     last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true},
-    mobile: { type: Number, required: true, unique: true,minlength:10 },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: Number, required: true, unique: true, minlength: 10 },
     password: { type: String, required: true },
     role: { type: String, required: true },
     status: { type: String, required: true }
